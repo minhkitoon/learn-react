d431e4ffdba96ccf3ff645af159ea422831f473c
NhatMinh <minh_nn@vietvang.net>
2023-03-01 20:56:15 +0700

    header 90%

---
+++ src/components/header.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './header.css';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+
+function Header(props) {
+    return (
+        <Container>
+            <header className='header'>
+            <a href="#" className='navbar-brand' id="logo">{props.logo}</a>
+                <Nav className='navbar navbar-fixed-top navbar-custom'>
+                    <Nav.Item>
+                        <Nav.Link href="/home">Home</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/about">About</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/gallery">Gallery</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/team">Team</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/services">Services</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/features">Features</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/pricing">Pricing</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href="/blog">Blog</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link className='discover-btn' href="/get-started">Get Started</Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </header>
+        </Container>
+    );
+}
+
+export default Header;
