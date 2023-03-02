979257f220c3d4acbc168e37d4071cc3352830e8
NhatMinh <minh_nn@vietvang.net>
2023-03-02 15:12:39 +0700

    add route

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
     <div className="App">
       <Header logo={'MinhReact'} />
       <Routes>
+          <Route path="/" element={ <Home /> } />
           <Route path="/home" element={ <Home /> } />
           <Route path="/about" element={ <About />} />
       </Routes>
